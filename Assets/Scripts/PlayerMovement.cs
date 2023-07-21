005eaa5930950164c9d68f90c8465cdee7b72bd6
Erik Medeiros <erikmedeiros4@gmail.com>
2023-07-21 16:43:17 -0300

    feat: rotate player towards movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,20 +4,22 @@ namespace PlayerMovement
 {
     public class PlayerMovement : MonoBehaviour
     {
-        [RangeAttribute(0f, 1f)] public float speed = 0.25f;
-        private CharacterController ct;
-
-        void Start()
-        {
-            ct = GetComponent<CharacterController>();
-        }
+        public float speed = 10f;
+        public float rotationSpeed = 720f;
 
         void Update()
         {
             float forward = Input.GetAxis("Vertical");
             float right = Input.GetAxis("Horizontal");
-            Vector3 directon = new(right, 0, forward);
-            ct.Move(directon * speed);
+            Vector3 moveVec = new Vector3(right, 0, forward) * speed * Time.deltaTime;
+
+            if (moveVec != Vector3.zero)
+            {
+                transform.Translate(moveVec, Space.World);
+                // https://www.ketra-games.com/2020/12/rotating-a-character-in-the-direction-of-movement-unity-game-tutorial.html
+                Quaternion toRotation = Quaternion.LookRotation(moveVec, Vector3.up);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+            }
         }
     }
 }
